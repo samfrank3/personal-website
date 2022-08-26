906f6e96ce18776555deae854c47ada8b1beb65a
Samuel <samfrank41802@gmail.com>
2022-08-26 15:21:53 -0400

    Successfully Deployed

---
+++ build/precache-manifest.6b677a54a7d399591a7082e4ca3110c5.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "c966731383f90b500198",
+    "url": "/static/css/main.91099199.chunk.css"
+  },
+  {
+    "revision": "c966731383f90b500198",
+    "url": "/static/js/main.c9667313.chunk.js"
+  },
+  {
+    "revision": "aa1ef92c5cf7728f9d0a",
+    "url": "/static/js/1.aa1ef92c.chunk.js"
+  },
+  {
+    "revision": "13681dfb1bd6971077b1",
+    "url": "/static/js/2.13681dfb.chunk.js"
+  },
+  {
+    "revision": "3ea3b0ffbd7375107cfd",
+    "url": "/static/js/runtime~main.3ea3b0ff.js"
+  },
+  {
+    "revision": "feb74da3fb378b705456a80ceca1c06e",
+    "url": "/static/media/Sam_head.feb74da3.png"
+  },
+  {
+    "revision": "23dc4f8e34ba52b5d46cf0509013c8b8",
+    "url": "/static/media/cydoc-logo.23dc4f8e.svg"
+  },
+  {
+    "revision": "731f037c7f81fcfe9fd61052c4f81bcf",
+    "url": "/static/media/roc-hci-logo.731f037c.png"
+  },
+  {
+    "revision": "9af72c1e377823a38f4b5b8cede548a7",
+    "url": "/static/media/uofr-logo.9af72c1e.png"
+  },
+  {
+    "revision": "95d7d3f0d046122816d455d0eb8e9ff6",
+    "url": "/static/media/CSUG.95d7d3f0.JPG"
+  },
+  {
+    "revision": "e705734c92a7f19b38d0ba98aeca8473",
+    "url": "/static/media/DandyHacks.e705734c.JPG"
+  },
+  {
+    "revision": "1ba23cf1f9c2c6b93f936c32fa520c29",
+    "url": "/static/media/UXD.1ba23cf1.JPG"
+  },
+  {
+    "revision": "b172fff6299f082ea78635486f2e4f83",
+    "url": "/static/media/PlayfairDisplay-SemiBold.b172fff6.woff2"
+  },
+  {
+    "revision": "6887b6f24414dbc612dbf42ccdc76b70",
+    "url": "/static/media/Roboto-Regular.6887b6f2.woff2"
+  },
+  {
+    "revision": "c89f1949aacb96563c67c2452b3f0338",
+    "url": "/index.html"
+  }
+];
