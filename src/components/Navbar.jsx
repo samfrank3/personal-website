914343100c76fd7da1f472e6b2bd716ce1427949
Samuel <samfrank41802@gmail.com>
2023-01-18 00:01:10 -0500

    finished the navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,5 +1,4 @@
 import React, {useState, useEffect} from 'react';
-import Hamburger from "./Hamburger";
 import styled from 'styled-components';
 
 const NavBar = styled.div`
@@ -11,6 +10,8 @@ position: fixed;
 z-index:5;
 top: 0;
 backdrop-filter: blur(10px);
+background: rgba(4,25,36,0.3);
+brightness: 0.1;
 -webkit-backdrop-filter: blur(10px);
 .navigation{
     list-style-type: none;
@@ -22,36 +23,26 @@ backdrop-filter: blur(10px);
     display: none;
 }
 @media (max-width: 1000px){
-    .hamburger{
-        display: flex;
-        float: right;
-        height: 40px;
-        padding-top: 10px;
-        margin-right: 10px;
-    }
-
     .navigation{
         padding-top: 40px;
         display: block;
         padding-left: 0;
-        margin: 0 5px 0 0;
-        background-color: #aaa;
+        margin: 0 0px 0 0;
         padding-bottom: 20px;
         border-bottom-left-radius: 10px;
         border-bottom-right-radius: 10px;
        
         overflow: hidden;
-        transition: max-height 0.5s ease;
+        transition: max-width 0.5s ease;
         border-top-left-radius: 2px;
         border-top-right-radius: 2px;
     }
 
     .button{
         width: 200px;
-        height: 60px;
-        background-color: #aaa;
         position: relative;
-        margin-top: -40px;
+        margin-top: -35px;
+        cursor: pointer;
     }
 }
 `;
@@ -71,9 +62,21 @@ font-size: 1.5rem;
     color: #75f0c3;
     border-bottom: 2px solid #75f0c3;
 }
+@media(max-width: 1000px){
+    &:hover {
+        color: #75f0c3;
+        border-left: 4px solid #75f0c3;
+        transition: all 0.2s cubic-bezier(.05,.27, .55,.98);
+        border-bottom: none;
+    }
+    &.active{
+        background-color: transparent;
+        color: #75f0c3;
+        border-left: 4px solid #75f0c3;
+        border-bottom: none;
+    }
+}
 `;
-
-
 
 const Navbar =() => {
     const [prevScrollPos, setPrevScrollPos] = useState(0);
@@ -84,10 +87,9 @@ const Navbar =() => {
     const toggleHamburger = () => {
         setHamburgerOpen(!hamburgerOpen)
         setHeightState(hamburgerOpen? '50px' : '325px')
-        console.log("clicked")
     }
 
-
+    // eslint-disable-next-line react-hooks/exhaustive-deps
     const handleScroll = () => {
         const currentScrollPos = window.pageYOffset;
         if(currentScrollPos <= 675){
@@ -112,29 +114,67 @@ const Navbar =() => {
     },[prevScrollPos, isActive, handleScroll]);
 
     return (
-        
         <NavBar>
-        
-        <Anchor href='#top' style={{float: 'left', fontFamily:'Playfair Display', fontSize: '25px'}}>Samuel Frank</Anchor>
-            <ul className= "navigation" style={{maxHeight: `${setHeight}`}}>
-                <div className="button" onClick={toggleHamburger}></div>
+            <Anchor href='#top' style={{float: 'left', fontFamily:'Playfair Display', fontSize: '25px'}}>Samuel Frank</Anchor>
+            <ul className= "navigation" >
+                <div className="button" onClick={toggleHamburger}>
+                    <div className="burger burger1" />
+                    <div className="burger burger2" />
+                    <div className="burger burger3" />
+                </div>
                 <li><Anchor id="navigation" className={isActive === 'Home' ? 'active': ''} href='#top'>Home</Anchor></li>
                 <li><Anchor id="navigation" className={isActive === 'About' ? 'active': ''} href='#About'>About</Anchor></li>
                 <li><Anchor id="navigation" className={isActive === 'Experience' ? 'active': ''} href='#Experience'>Experience</Anchor></li>
                 <li><Anchor id="navigation" className={isActive === 'Projects' ? 'active': ''} href='#Projects'>Projects</Anchor></li>
                 <li><Anchor id="navigation" className={isActive === 'Leadership' ? 'active': ''} href="#Leadership">Leadership</Anchor></li>
             </ul>
-            
-            
             <style jsx> {`
                 @media(max-width: 1000px) {
                     #navigation{
-                        // display: ${hamburgerOpen? 'block': 'none'};
-                        display: ${setHeight =='300px'? 'block': 'inline-block'};
-                        
-                        
+                        display: ${hamburgerOpen? 'block': 'none'};
                         width: 200px;
                     }
+                    .navigation{
+                        max-width: ${setHeight};
+                        background-color: ${hamburgerOpen? '#2a3b42' : ''};
+                    }
+
+                    .button{
+                        display: flex;
+                        flex-flow: column nowrap;
+                        justify-content: space-around;
+                        height: 2.25rem;
+                        padding-left: 10px;
+                        padding-top: 5px;
+                        background-color: ${hamburgerOpen? '#2a3b42' : ''};
+                    }
+
+                    .burger{
+                        background-color: #F9FEFF;
+                        width: 2rem;
+                        height: 0.25rem;
+                        border-radius: 10px;
+                        transform-origin: 1px;
+                        transition: all 0.3s linear;
+                    }
+                    .burger1{
+                        transform: ${ hamburgerOpen ? 'rotate(45deg)' : 'rotate(0)'};
+                        {/* background-color: ${hamburgerOpen? '#75f0c3': '#F9FEFF'} */}
+                        background-color: #F9FEFF;
+                    }
+
+                    .burger2{
+                        transform: ${hamburgerOpen ? 'translateX(100%)': 'translateX(0)'};
+                        opacity: ${hamburgerOpen? '0': '1'};
+                        background-color: #F9FEFF;
+                    }
+
+                    .burger3{
+                        transform: ${ hamburgerOpen? 'rotate(-45deg)' : 'rotate(0)'};
+                        {/* background-color: ${hamburgerOpen? '#75f0c3': '#F9FEFF'} */}
+                        background-color: #F9FEFF;
+                    }
+
                 }
             `}</style>
         </NavBar>
